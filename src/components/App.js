5bccd468de119c1589db916e72d4c4fc208f49c2
菅田祐介 <ys@MBA-2022.local>
2022-04-04 01:49:12 +0900

    create config

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 
 import SignIn from './SignIn';
-import Main from './Main'
+import Main from './Main';
+import config from '../config.json';
 
 export default () => {
   var [name, setName] = useState('');
-  console.log({ name });
 
-  if (name === ''){
+  if (config.signInEnabled && name === ''){
     return <SignIn setName={setName} />;
   }else{
     return <Main name={name}/>
